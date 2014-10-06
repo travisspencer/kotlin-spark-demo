5a2ba278fc53621343a4b6edfc8fdfad04134af5
Travis Spencer <travis.spencer@twobotechnologies.com>
2014-10-06 08:43:16 +0200

    Removed boilerplate code

--- src/main/java/SpartJavaSample/ContainerComposer.java
+++ src/main/java/SpartJavaSample/ContainerComposer.java
@@ -8,19 +8,12 @@ class ContainerComposer
 {
     public static void composeApplication(DefaultPicoContainer appContainer)
     {
-        //To change body of created methods use File | Settings | File Templates.
         appContainer.addComponent(AuthorizeController.class);
         appContainer.addComponent(TokenController.class);
         appContainer.addComponent(LoginController.class);
     }
 
-    public static void composeSession(MutablePicoContainer container)
-    {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+    public static void composeSession(MutablePicoContainer container) { }
 
-    public static void composeRequest(MutablePicoContainer container)
-    {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+    public static void composeRequest(MutablePicoContainer container) { }
 }
